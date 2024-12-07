c04584558275a1e1910f0d70a1aaeb9e7ea10987
nikhilkhanchandani <nikhilkhanchandani@gmail.com>
2024-12-07 13:38:04 -0800

    Brain  Tumor

---
+++ braintumor/brain-tumor-ui/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BrainTumorApp from "./App";
+
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrainTumorApp />
+  </React.StrictMode>
+);
